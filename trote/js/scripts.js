41da08e4f156506dae5e0069dca4e46bf9ceda8b
brunowhy <brunogamerlivre@gmail.com>
2025-03-24 20:44:25 -0300

    primeiro site

---
+++ trote/js/scripts.js
@@ -0,0 +1,6 @@
+function calcular(){
+    let acaoSocial = document.getElementById("acaoSocial").value
+    let homenagem = document.getElementById("homenagem").value
+    let soma = Number(acaoSocial) + Number(homenagem)
+    alert(soma)
+}
