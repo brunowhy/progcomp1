e8acfd714e0dc094aae197849d1dd7bc6611c922
brunowhy <brunogamerlivre@gmail.com>
2025-03-26 22:08:48 -0300

    primeiro site

--- trote/js/scripts.js
+++ trote/js/scripts.js
@@ -1,6 +1,14 @@
 function calcular(){
+    let soma = 0
     let acaoSocial = document.getElementById("acaoSocial").value
+    soma = soma + Number(acaoSocial)
     let homenagem = document.getElementById("homenagem").value
-    let soma = Number(acaoSocial) + Number(homenagem)
-    alert(soma)
+    soma = soma + Number(homenagem)
+    let leite = document.getElementById("leite").value
+    soma = soma + Number(2 * Number(leite))
+    let kitAvulso = document.getElementById("kitAvulso").value
+    soma = soma + Number(30 * Number(kitAvulso))
+    let suplementoAvulso = document.getElementById("suplementoAvulso").value
+    soma = soma + Number(15 * Number(suplementoAvulso))
+    document.getElementById("soma").innerHTML = soma.toFixed(2)
 }
